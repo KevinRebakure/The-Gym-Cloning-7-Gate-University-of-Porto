9247713b4c5daddb76fa790195c26f7441541c9b
KevinRebakure <kevinrebakure@gmail.com>
2024-05-17 17:56:31 +0200

    updated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     extend: {
       colors: {
         dark: "#333333",
-        gray: "#d5d5d5",
-        graydarker: "#999999",
+        mygray: "#d5d5d5",
+        mygraydarker: "#999999",
         brown: "#a18a4d"
       },
     },
